d9bb526758b85784c68449aea7dd434ff4d07d92
KvngAlka <kingsford499@gmail.com>
2022-12-17 23:07:05 +0000

    modified woker get notification endpoint

---
+++ funtions.js
@@ -0,0 +1,46 @@
+import Services from "./models/Services.js";
+import Subservices from "./models/Subservices.js";
+import User from "./models/User.js";
+
+
+export const convertService = async(list)=>{
+
+    const newPosts =  await Promise.all(
+        list.map(async(item) =>{
+
+            const {serviceId, subServiceId} = item;
+            const services = await Services.find({code : serviceId});
+            const subServices = await Subservices.find({parentCode : serviceId, code : subServiceId})
+
+            const service = services[0]?._doc;
+            const subService = subServices[0]?._doc;
+            const serviceName = service?.name;
+
+
+            return {
+                ...item._doc,
+                ...service,
+                ...subService,
+                serviceName
+                
+            }
+
+        })
+
+    ) 
+
+    return 
+
+}
+
+
+
+export async function getUserById(id){
+
+    const user = await User.find({_id : id});
+
+    if(user) return user[0];
+
+    else return null
+
+}
--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,7 @@ import { userExists, validateClientPost } from "../validation.js";
 import User from "../models/User.js";
 import Services from "../models/Services.js";
 import Subservices from "../models/Subservices.js";
+import { getUserById } from "../funtions.js";
 
 
 const postRouter  = Router();
@@ -125,6 +126,80 @@ postRouter.post("/get/clientposts", authToken,async(req,res)=>{
     }
 
 })
+
+
+
+//FOR WORKERS
+
+
+postRouter.post("/worker/get/notifications", authToken, async(req,res)=>{
+    const {workerId} = req.body;
+
+    const posts = await ClientPost.find();
+    const workerDetails = await User.find({_id : workerId});
+
+    function containsObject(obj, list) {
+        var i;
+        let found = false;
+
+        for (i = 0; i < list.length; i++) {
+
+            
+            if (!found && JSON.stringify(list[i]) === JSON.stringify(obj) ){
+                found =  true;
+            }
+        }
+    
+        return found;
+    }
+
+    if(workerDetails[0].skills){
+        const skills = workerDetails[0].skills
+        const newSkills = skills.map(skill => ({serviceId : skill.serviceId, subServiceId : skill.subServiceId}))
+
+
+        const postsToSend = posts.filter(
+            post => (
+                
+                !post.isAccepted &&
+                containsObject({serviceId : post.serviceId, subServiceId : post.subServiceId}, newSkills)
+            )
+        )
+
+        const newPostsToSend =  await Promise.all(
+            postsToSend.map(async(post) =>{
+
+                const {serviceId, subServiceId, ownerId} = post;
+                const services = await Services.find({code : serviceId});
+                const subServices = await Subservices.find({parentCode : serviceId, code : subServiceId});
+                const nameOfClient = await getUserById(ownerId)
+
+                console.log(nameOfClient)
+
+                const service = services[0]?._doc;
+                const subService = subServices[0]?._doc;
+                const serviceName = service?.name;
+    
+    
+                return {
+                    ...post._doc,
+                    ...service,
+                    ...subService,
+                    nameOfClient : nameOfClient.fullName,
+                    serviceName
+                    
+                }
+    
+            })
+
+        ) 
+
+        res.send({code : 201, data : newPostsToSend})
+    }
+
+    
+})
+
 
 
 
